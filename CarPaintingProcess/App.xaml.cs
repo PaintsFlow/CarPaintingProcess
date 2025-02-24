9794c92148a700fc43f335ef46479cc14642a619
salt338-03 <salt338@naver.com>
2025-02-24 10:02:02 +0900

    초기 커밋.

---
+++ CarPaintingProcess/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Prism.Ioc;
+using Prism.DryIoc;
+using System.Windows;
+using CarPaintingProcess.Views;
+using Prism.Regions;  
+
+namespace CarPaintingProcess
+{
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<ProcessAView>("ProcessAView");
+            containerRegistry.RegisterForNavigation<ProcessBView>("ProcessBView");
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            // Prism에서 제공하는 DI 컨테이너로부터 RegionManager 가져오기
+            var regionManager = Container.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion("LeftSideRegion", typeof(SideBarView));
+
+        }
+    }
+}
---
+++ CarPaintingProcess/ViewModels/SideBarViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace CarPaintingProcess.ViewModels
+{
+    public class SideBarViewModel : BindableBase
+    {
+        private readonly IRegionManager _regionManager;
+
+        public DelegateCommand<string> NavigateCommand { get; private set; }
+
+        public SideBarViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+
+            NavigateCommand = new DelegateCommand<string>(Navigate);
+        }
+
+        private void Navigate(string viewName)
+        {
+            if (!string.IsNullOrEmpty(viewName))
+            {
+                _regionManager.RequestNavigate("MainRegion", viewName);
+            }
+        }
+    }
+}
